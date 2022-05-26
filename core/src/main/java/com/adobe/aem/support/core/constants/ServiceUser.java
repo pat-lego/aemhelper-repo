d93fa93088201e01343f6ab4cd0c06713197a083
Patrique Legault <patrique.legault@gmail.com>
2022-05-26 11:15:56 -0400

    Adding simple endpoint

--- core/src/main/java/com/adobe/aem/support/core/constants/ServiceUser.java
+++ core/src/main/java/com/adobe/aem/support/core/constants/ServiceUser.java
@@ -1,5 +1,10 @@
 package com.adobe.aem.support.core.constants;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.sling.api.resource.ResourceResolverFactory;
+
 public class ServiceUser {
 
     public ServiceUser() {
@@ -9,4 +14,9 @@ public class ServiceUser {
 
     public static final String AEM_SUPPORT_SUBSERVICE = "aemsupport";
     
+    public static final Map getAEMSupportAuthMap() {
+        Map auth = new HashMap<Object, Object>();
+        auth.put(ResourceResolverFactory.SUBSERVICE, AEM_SUPPORT_SUBSERVICE);
+        return auth;
+    }
 }
